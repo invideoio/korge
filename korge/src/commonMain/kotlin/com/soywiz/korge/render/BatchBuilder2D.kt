ecb0da10f847216564b17c03e2eddd2701c49898
Sahil Bajaj <sahilbajaj.nc@gmail.com>
2021-10-29 11:17:54 +0530

    Don't flip horizontally when flip flag is set

--- korge/src/commonMain/kotlin/com/soywiz/korge/render/BatchBuilder2D.kt
+++ korge/src/commonMain/kotlin/com/soywiz/korge/render/BatchBuilder2D.kt
@@ -744,7 +744,7 @@ class BatchBuilder2D constructor(
         if (flipRenderTexture && ag.renderingToTexture) {
             projMat.setToOrtho(tempRect.setBounds(0, ag.currentHeight, ag.currentWidth, 0), -1f, 1f)
         } else if (ag.flipRender) {
-            projMat.setToOrtho(tempRect.setBounds(ag.currentWidth, ag.currentHeight, 0, 0), -1f, 1f)
+            projMat.setToOrtho(tempRect.setBounds(0, ag.currentHeight, ag.currentWidth, 0), -1f, 1f)
         } else {
             projMat.setToOrtho(tempRect.setBounds(0, 0, ag.currentWidth, ag.currentHeight), -1f, 1f)
         }
